636788842983eabae557e9ea9f3ae5b3ae97a930
Ricard <r.molina91@gmail.com>
2018-01-13 14:07:26 +0100

    Routing from navbar to navbar and search component. RouterLink. RouterOutlet y RouterLinkActive.

--- SpotyApplication/src/app/app.module.ts
+++ SpotyApplication/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Fichero de rutas
+import { appRouting } from './app.routes';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -16,7 +19,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    appRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
